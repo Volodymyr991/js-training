1a76a14b367c868657f6a08694a43aff9b254899
Volodymyr991 <vovand12vovand12@gmail.com>
2023-07-08 11:14:39 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function calculateEngravingPrice(message, pricePerWord) {
+    // Change code below this line
+let totalPrice;
+
+const ww = message.split(" ");
+
+totalPrice = message * [pricePerWord];
+
+console.log(message);
+
+return pricePerWord;
+    // Change code above this line
+ }
